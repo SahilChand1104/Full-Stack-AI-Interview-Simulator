b5cef90351f8d9fa37472e2b7682d27b85f951d5
Sahil <Sahil.Chand1104@gmail.com>
2024-09-25 14:43:50 -0700

    Added a button

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '#D8A444',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
