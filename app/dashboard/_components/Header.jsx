64819a2eab15cd8fbeaade27038dcab7378267a6
Sahil <Sahil.Chand1104@gmail.com>
2024-09-26 11:56:37 -0700

    added indicator

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,14 +1,25 @@
-
+"use client"
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 function Header() {
+
+    const path = usePathname();
+    useEffect(()=>{
+        console.log(path)
+    }, [])
+
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-md'>
         <Image src ={'/logo.svg'} width={160} height={100} alt ='logo'/>
         <ul className='flex gap-6'>
-            <li className='hover:text-primary hover:font-bold transition-all cursor-pointer'>Dashboard</li>
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+            ${path=='/dashboard'&&'text-primary font-bold'}
+            `}
+            
+            >Dashboard</li>
             <li className='hover:text-primary hover:font-bold transition-all cursor-pointer'>Questions</li>
             <li className='hover:text-primary hover:font-bold transition-all cursor-pointer'>Upgrade</li>
             <li className='hover:text-primary hover:font-bold transition-all cursor-pointer'>How it works?</li>
