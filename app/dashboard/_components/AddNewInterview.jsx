e8eac35db6d3f71f721a00575f9999d59cf31f65
Sahil <Sahil.Chand1104@gmail.com>
2024-09-29 14:36:44 -0700

    fixed parameter bug

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -18,6 +18,7 @@ import { MockInterview } from '@/utils/schema'
 import { v4 as uuidv4 } from 'uuid';
 import { useUser } from '@clerk/nextjs'
 import moment from 'moment'
+import { useRouter } from 'next/navigation'
   
 
 function AddNewInterview() {
@@ -27,6 +28,7 @@ function AddNewInterview() {
     const [jobExperience, setJobExperience] = useState(false);
     const [Loading, setLoading] = useState(false);
     const [JsonResponse,setJsonResponse] = useState([]);
+    const router =useRouter();
     const {user} = useUser();
 
     const onSubmit = async(e)=>{
@@ -57,6 +59,7 @@ function AddNewInterview() {
       console.log("Inserted ID:", resp);
       if(resp){
         setOpenDialog(false);
+        router.push('/dashboard/interview/'+resp[0]?.mockId)
       }
     }
     else{
