d5df852ca6e3bba45ee6663b6b22a7cba6c34d61
Sahil <Sahil.Chand1104@gmail.com>
2024-09-29 18:55:33 -0700

    Allows Webcam permissions

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -1,13 +1,16 @@
 "use client"
+import { Button } from '@/components/ui/button'
 import { db } from '@/utils/db'
 import { MockInterview } from '@/utils/schema'
 import { eq } from 'drizzle-orm'
+import { WebcamIcon } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import Webcam from 'react-webcam'
 
 function Interview({params}) {
 
     const [interviewData, setInterviewData]=useState();
+    const [webCamEnabled, setWebCamEnabled]=useState(false);
 
     useEffect(()=>{
         console.log(params.interviewId)
@@ -26,7 +29,21 @@ function Interview({params}) {
     <div className='my-10 flex justify-center flex-col items-center'>
         <h2 className='font-bold text-2xl'>Let's Get Started</h2>
         <div>
-        <Webcam />
+        {webCamEnabled? <Webcam
+        onUserMedia={()=>setWebCamEnabled(true)}
+        onUserMediaError={()=>setWebCamEnabled(false)}
+        style={{
+          height:300,
+          width:300
+        }} />
+        :
+        <>
+        <WebcamIcon className='h-72 w-full my-7 p-20 bg-secondary rounded-lg border'/>
+        <Button onClick={()=>setWebCamEnabled(true)}>Enable Web Cam and Microphone</Button>
+        </>
+        
+        }
+
         </div>
     </div>
   )
