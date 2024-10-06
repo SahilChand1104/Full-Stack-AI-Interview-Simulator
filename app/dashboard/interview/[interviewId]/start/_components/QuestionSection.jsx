471907bc74123fe48d69cb27ba3f5ed4150101b7
Sahil <Sahil.Chand1104@gmail.com>
2024-10-05 18:17:15 -0700

    centered the text

--- app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
@@ -5,7 +5,7 @@ function QuestionSection({mockInterviewQuestion}) {
     <div className ='p-5 border rounded-lg'>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
             {mockInterviewQuestion&&mockInterviewQuestion.map((question,index)=>(
-                <h2 className='p-2 bg-secondary rounded-full'>Question #{index+1}</h2>
+                <h2 className='p-2 bg-secondary rounded-full text-xs md:text-sm text-center'>Question #{index+1}</h2>
             ))}
         </div>
 
