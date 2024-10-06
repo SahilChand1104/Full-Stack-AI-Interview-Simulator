cc7d93bd7d4c82591da61f711adc5df1efae3325
Sahil <Sahil.Chand1104@gmail.com>
2024-10-05 18:02:40 -0700

    Got display to work in console

--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+"use client"
+import { db } from '@/utils/db';
+import { MockInterview } from '@/utils/schema';
+import { eq } from 'drizzle-orm';
+import React, { useEffect, useState } from 'react'
 
-function StartInterview() {
+function StartInterview({params}) {
+
+    const [interviewData, setInterviewData]=useState();
+    const [mockInterviewQuestion, setMockInterviewQuestion]=useState();
+    useEffect(()=>{
+        GetInterviewDetails();
+
+    }, []);
+
+    const GetInterviewDetails=async()=>{
+        const result=await db.select().from(MockInterview)
+        .where(eq(MockInterview.mockId, params.interviewId))
+
+        //console.log(result);
+        const jsonMockResp=JSON.parse(result[0].jsonMockResp)
+        console.log(jsonMockResp)
+        setMockInterviewQuestion(jsonMockResp);
+        setInterviewData(result[0]);
+        
+    }
+
+
   return (
     <div>StartInterview</div>
   )
