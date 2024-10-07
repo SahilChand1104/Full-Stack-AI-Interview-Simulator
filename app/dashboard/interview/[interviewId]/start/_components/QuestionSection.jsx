274305876a4c108594ba49eafdafd0648f127969
Sahil <Sahil.Chand1104@gmail.com>
2024-10-07 14:58:16 -0700

    question is displayed

--- app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
 function QuestionSection({mockInterviewQuestion, activeQuestionIndex}) {
-  return (
+  return mockInterviewQuestion&&(
     <div className ='p-5 border rounded-lg'>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
             {mockInterviewQuestion&&mockInterviewQuestion.map((question,index)=>(
                 <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex==index&&'bg-black text-primary'}`}>Question #{index+1}</h2>
             ))}
-        </div>
 
+            
+        </div>
+        <h2>{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
     </div>
   )
 }
