7e2ab24bd928a5c04b5ad4e736f5ed2bbb820f03
Sahil <Sahil.Chand1104@gmail.com>
2024-10-07 15:06:25 -0700

    adding some styling to the note section

--- app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
@@ -1,16 +1,23 @@
+import { Lightbulb } from 'lucide-react'
 import React from 'react'
 
 function QuestionSection({mockInterviewQuestion, activeQuestionIndex}) {
   return mockInterviewQuestion&&(
-    <div className ='p-5 border rounded-lg'>
+    <div className ='p-5 border rounded-lg my-10'>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
             {mockInterviewQuestion&&mockInterviewQuestion.map((question,index)=>(
                 <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex==index&&'bg-black text-primary'}`}>Question #{index+1}</h2>
             ))}
-
-            
         </div>
         <h2 className='my-5 text-md md:text-lg'>{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
+
+        <div className='border rounded-lg p-5 bg-red-100'>
+          <h2 className ='flex gap-2 items-center text-red-800'>
+            <Lightbulb />
+            <strong>Note:</strong>
+          </h2>
+        </div>
+
     </div>
   )
 }
