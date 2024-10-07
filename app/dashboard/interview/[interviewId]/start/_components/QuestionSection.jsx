d47701ed3ad53ec60560f116bbaf54e0abb3c74e
Sahil <Sahil.Chand1104@gmail.com>
2024-10-07 15:16:37 -0700

    fixed margin issue on the note section

--- app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
@@ -11,12 +11,12 @@ function QuestionSection({mockInterviewQuestion, activeQuestionIndex}) {
         </div>
         <h2 className='my-5 text-md md:text-lg'>{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
 
-        <div className='border rounded-lg p-5 bg-red-100'>
+        <div className='border rounded-lg p-5 bg-red-100 mt-20'>
           <h2 className ='flex gap-2 items-center text-red-800'>
             <Lightbulb />
             <strong>Note:</strong>
           </h2>
-          <h2>{process.env.NEXT_PUBLIC_QUESTION_NOTE}</h2>
+          <h2 className='text-sm text-red-800 my-2'>{process.env.NEXT_PUBLIC_QUESTION_NOTE}</h2>
         </div>
 
     </div>
