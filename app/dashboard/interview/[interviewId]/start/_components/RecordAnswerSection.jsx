b7533770b3090c96e90bc7ed046bb1f2fd06fc1a
Sahil <Sahil.Chand1104@gmail.com>
2024-10-09 17:07:13 -0700

    added a button to record the answer

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -1,23 +1,26 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import React from 'react'
 import Webcam from 'react-webcam'
 
 function RecordAnswerSection() {
   return (
-    <div className='flex flex-col my-20 justify-center items-center bg-orange rounded-lg p-5'>
-        <Image src={'/webcam.png'} width ={200} height={200}
-        className='absolute' />
-        <Webcam
-        mirrored={true}
-        style={{
-            height:300,
-            width:'100%',
-            zIndex:10,
+    <div>
+        <div className='flex flex-col my-20 justify-center items-center bg-orange rounded-lg p-5'>
+            <Image src={'/webcam.png'} width ={200} height={200}
+            className='absolute' />
+            <Webcam
+            mirrored={true}
+            style={{
+                height:300,
+                width:'100%',
+                zIndex:10,
+            }}
+            />
 
-        }}
-        />
-
-        
+            
+        </div>
+        <Button>Record Answer</Button>
     </div>
   )
 }
