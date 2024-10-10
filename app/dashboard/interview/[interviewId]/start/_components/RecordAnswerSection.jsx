1109e914739931598d95a4a567a86fa7e92caa94
Sahil <Sahil.Chand1104@gmail.com>
2024-10-09 17:15:53 -0700

    introduced microphone option

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -1,9 +1,24 @@
+"use client"
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import React from 'react'
+import useSpeechToText from 'react-hook-speech-to-text'
 import Webcam from 'react-webcam'
 
 function RecordAnswerSection() {
+    const {
+        error,
+        interimResult,
+        isRecording,
+        results,
+        startSpeechToText,
+        stopSpeechToText,
+      } = useSpeechToText({
+        continuous: true,
+        useLegacyResults: false
+      });
+
+
   return (
     <div className='flex items-center justify-center flex-col'>
         <div className='flex flex-col mt-20 justify-center items-center bg-orange rounded-lg p-5'>
@@ -21,6 +36,17 @@ function RecordAnswerSection() {
             
         </div>
         <Button className='my-10'>Record Answer</Button>
+
+        <h1>Recording: {isRecording.toString()}</h1>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+      <ul>
+        {results.map((result) => (
+          <li key={result.timestamp}>{result.transcript}</li>
+        ))}
+        {interimResult && <li>{interimResult}</li>}
+      </ul>
     </div>
   )
 }
